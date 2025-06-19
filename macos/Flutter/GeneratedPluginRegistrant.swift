5e24bd5e6f417bd3b8562e0dc7dbccaaf152eefa
Juan Pablo Ruiz Partida <jppart40@gmail.com>
2025-06-19 11:18:32 +0100

    Podfile, pubspec files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import bonsoir_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
 }
